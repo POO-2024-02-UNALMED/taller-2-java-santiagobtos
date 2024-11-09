34375d747a6ea32eff849b0b1bde906dc41c10d9
santiagobtos <sbarrientosm@unal.edu.co>
2024-11-09 23:24:30 +0000

    9

--- src/test/java/test/Auto.java
+++ src/test/java/test/Auto.java
@@ -25,26 +25,19 @@ public class Auto{
 
 
     }
-    String verificarIntegridad(){
-        if (this.motor.registro==this.registro) {
-            int o=motor.registro;
-            for(int i=0; i< this.asientos.length; i++){
-                if (this.asientos[i] != null ) {
-                    if (this.asientos[i].registro==o) {
-                        return "Auto original";
-                
-                    
-                    }else{
+    String verificarIntegridad() {
+        if (this.motor.registro == this.registro) {
+            int o = motor.registro;
+            for (int i = 0; i < this.asientos.length; i++) {
+                if (this.asientos[i] != null) {
+                    if (this.asientos[i].registro != o) {
                         return "Las piezas no son originales";
                     }
-                
                 }
             }
-            
-        }else{
+            return "Auto original";
+        } else {
             return "Las piezas no son originales";
         }
     }
-        
-
 }
